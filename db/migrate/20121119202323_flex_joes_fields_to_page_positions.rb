8e56c41196e5457550795f048a619576b474f257
Joe Kurleto <joedoku@gmail.com>
2012-11-20 12:50:28 -0500

    Added migration for additional page_position fields

---
+++ db/migrate/20121119202323_flex_joes_fields_to_page_positions.rb
@@ -0,0 +1,8 @@
+class AddFlexFieldsToPagePositions < ActiveRecord::Migration
+  def change
+    add_column :refinery_page_positions, :refinery_resource_id, :integer
+    add_column :refinery_page_positions, :refinery_resource_type, :string
+    add_column :refinery_page_positions, :title_attribute, :string
+    add_column :refinery_page_positions, :custom_url, :string
+  end
+end
