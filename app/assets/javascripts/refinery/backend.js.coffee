7eb2e5a76f6f6d1d8e8f480a6b5fa7f2d4ba546d
Johan Frølich <johanfrolich@gmail.com>
2013-01-05 23:49:44 +0100

    added destroy functionallity

--- app/assets/javascripts/refinery/backend.js.coffee
+++ app/assets/javascripts/refinery/backend.js.coffee
@@ -1,12 +1,27 @@
 jQuery =>
-  $('#sortable_list, .sortable_list').addClass("reordering")
-  list_reorder.sortable_list.nestedSortable("enable")
-  
-  $('.header').click ->
-    $(this).siblings('.body').slideToggle()
+	$('#form-container').css('min-height', $('#left_actions').height() + "px")
 
-  $('a.add_link').click ->
-  	data = $.post($(this).data('url'))
-  	alert(data)
+	$('#sortable_list, .sortable_list').addClass("reordering")
+	list_reorder.sortable_list.nestedSortable("enable")
 
-  $('#form-container').css('min-height', $('#left_actions').height() + "px")
+	$('#sortable_list').on 'click', '.header', ->
+		$(this).siblings('.body').slideToggle()
+
+	$('#sortable_list').on 'click', '.remove', ->
+		$(this).parents('.pp-link').fadeOut().find('input.destroy').val('true')
+
+	$('#page_menu_form').submit (e) ->
+		save_structure($(this))
+
+save_structure = (form) ->
+	list_reorder.sortable_list.nestedSortable("disable")
+
+	$('#sortable_list, .sortable_list').removeClass("reordering")
+
+	if list_reorder.update_url != null
+		serialized_lists = list_reorder.sortable_list.serializelist()
+
+		for elem in serialized_lists.split('&')
+			if elem.length
+				input = $("<input>").attr("type", "hidden").attr("name", elem.split('=')[0]).val(elem.split('=')[1])
+				form.append(input)
--- app/controllers/refinery/admin/page_menus_controller.rb
+++ app/controllers/refinery/admin/page_menus_controller.rb
@@ -6,6 +6,7 @@ module Refinery
       
       before_filter :find_menu_pages, only: [:new, :edit]
       before_filter :find_page_positions, only: [:edit, :update]
+      before_filter :set_page_positions, only: [:create, :update]
       
       def new
         @page_menu = PageMenu.new
@@ -15,9 +16,52 @@ module Refinery
         @pages = Refinery::Page.all
       end
       
-      
-      
       private
+
+      # Based upon http://github.com/matenia/jQuery-Awesome-Nested-Set-Drag-and-Drop
+      def set_page_positions
+        previous = nil
+        params[:ul].each do |_, list|
+          list.each do |index, hash|
+            moved_item_id = hash['id'].split(/page_position\_?/).reject(&:empty?).first
+
+            if moved_item_id
+              current_position = @page_menu.positions.find(moved_item_id) # Scope?
+
+              if current_position.respond_to?(:move_to_root)
+                if previous.present?
+                  current_position.move_to_right_of(@page_menu.positions.find(previous)) #SCOPE?
+                else
+                  current_position.move_to_root
+                end
+              else
+                current_position.update_attributes :position => index
+              end
+
+              if hash['children'].present?
+                update_child_page_positions(hash, current_position)
+              end
+
+              previous = moved_item_id
+            end
+          end
+        end if params[:ul].present?
+
+        PagePosition.rebuild!
+      end
+
+      def update_child_page_positions(_node, position)
+        list = _node['children']['0']
+        list.each do |index, child|
+          child_id = child['id'].split(/page_position\_?/).reject(&:empty?).first
+          child_position = @page_menu.positions.find(child_id) # Scoped?
+          child_position.move_to_child_of(position)
+
+          if child['children'].present?
+            update_child_page_positions(child, child_position)
+          end
+        end
+      end
       
       def find_menu_pages
         @pages_in_menu = Refinery::Page.in_menu
